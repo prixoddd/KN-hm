9331b98dc0d13cf3b655eab2bdd9d360761f08cd
Nikita <nikita.derjugin@gmail.com>
2024-02-17 18:52:20 +0200

    v 3.9 deploy on vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/KN-hm/',
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
