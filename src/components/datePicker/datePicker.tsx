9cba7dcb2f547366c87ecb3d29b7f8d56addcb2a
Nikita <nikita.derjugin@gmail.com>
2024-02-17 19:06:17 +0200

    v 3.9 deploy on vercel

--- src/components/datePicker/datePicker.tsx
+++ src/components/datePicker/datePicker.tsx
@@ -3,7 +3,7 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo'
-import * as dayjs from 'dayjs'
+import dayjs from 'dayjs'
 
 import 'dayjs/locale/en'
 
